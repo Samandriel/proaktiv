c59a3aa8631294d7e0f3adbc45aa0807189912aa
harris <harris.kunakira@gmail.com>
2023-06-30 21:18:12 +0700

    wip: appBar and sidebar

--- src/components/layout/AppBar.tsx
+++ src/components/layout/AppBar.tsx
@@ -1,4 +1,6 @@
 import { Icon } from '@iconify/react';
+import styles from './_layout.module.scss';
+
 import {
   Avatar,
   Group,
@@ -10,44 +12,46 @@ import {
 
 export const AppBar = () => {
   return (
-    <Header height={rem(60)} className="px-6">
-      <Group>
-        <div>Logo</div>
-      </Group>
-      <Group position="right">
-        <Menu
-          width={260}
-          position="bottom-end"
-          transitionProps={{ transition: 'pop-top-right' }}
-          withinPortal
-        >
-          {/* onClose={() => setUserMenuOpened(false)}
+    <Header height={rem(60)} className={styles.appBar}>
+      <div className="w-full h-full flex items-center">
+        <div className={styles.appBarItems}>
+          <h1>ProAktiv</h1>
+        </div>
+        <div className={styles.appBarItemsEnd}>
+          <Menu
+            width={260}
+            position="bottom-end"
+            transitionProps={{ transition: 'pop-top-right' }}
+            withinPortal
+          >
+            {/* onClose={() => setUserMenuOpened(false)}
           onOpen={() => setUserMenuOpened(true)} */}
-          <Menu.Target>
-            <UnstyledButton>
-              <Group spacing={7}>
-                <Avatar src={''} alt={''} radius="xl" size={20} />
-              </Group>
-            </UnstyledButton>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Item>Liked posts</Menu.Item>
-            <Menu.Item>Saved posts</Menu.Item>
-            <Menu.Item>Your comments</Menu.Item>
+            <Menu.Target>
+              <UnstyledButton>
+                <Group spacing={7}>
+                  <Avatar src={''} alt={''} radius="xl" size={20} />
+                </Group>
+              </UnstyledButton>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item>Liked posts</Menu.Item>
+              <Menu.Item>Saved posts</Menu.Item>
+              <Menu.Item>Your comments</Menu.Item>
 
-            <Menu.Label>Settings</Menu.Label>
-            <Menu.Item>Account settings</Menu.Item>
-            <Menu.Item>Change account</Menu.Item>
-            <Menu.Item>Logout</Menu.Item>
+              <Menu.Label>Settings</Menu.Label>
+              <Menu.Item>Account settings</Menu.Item>
+              <Menu.Item>Change account</Menu.Item>
+              <Menu.Item>Logout</Menu.Item>
 
-            <Menu.Divider />
+              <Menu.Divider />
 
-            <Menu.Label>Danger zone</Menu.Label>
-            <Menu.Item>Pause subscription</Menu.Item>
-            <Menu.Item>Delete account</Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      </Group>
+              <Menu.Label>Danger zone</Menu.Label>
+              <Menu.Item>Pause subscription</Menu.Item>
+              <Menu.Item>Delete account</Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
+      </div>
     </Header>
   );
 };
--- src/components/layout/AppLeftSidebar.tsx
+++ src/components/layout/AppLeftSidebar.tsx
@@ -1,10 +1,39 @@
+import { Icon } from '@iconify/react';
 import { Navbar, rem } from '@mantine/core';
+import Link from 'next/link';
+import styles from './_layout.module.scss';
+
+const menuItems = [
+  { label: 'Home', icon: 'bx:bx-home', link: '/' },
+  { label: 'Trackers', icon: 'bx:bx-bar-chart-alt-2', link: '/trackers' },
+  { label: 'Todo', icon: 'bx:bx-list-check', link: '/todos' },
+  { label: 'Notes', icon: 'bx:bx-notepad', link: '/notes' },
+  { label: 'Toolbox', icon: 'bx:bx-wrench', link: '/toolbox' },
+];
 
 export const AppLeftSidebar = () => {
+  const menuList = menuItems.map((item) => {
+    return (
+      <Link
+        href={item.link}
+        key={item.label}
+        className={styles.appLeftSidebarMenuItem}
+      >
+        <Icon
+          className={styles.appLeftSidebarMenuItemIcon}
+          icon={item.icon}
+          width={rem(24)}
+          height={rem(24)}
+        />
+        <span>{item.label}</span>
+      </Link>
+    );
+  });
+
   return (
-    <Navbar width={{ base: rem(300) }}>
+    <Navbar width={{ base: rem(300) }} className={styles.appLeftSidebar}>
       <Navbar.Section>{}</Navbar.Section>
-      <Navbar.Section grow>{}</Navbar.Section>
+      <Navbar.Section grow>{menuList}</Navbar.Section>
       <Navbar.Section>{}</Navbar.Section>
     </Navbar>
   );
