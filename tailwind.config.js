e299390e4ac865ee8f96c9f5ed0dde300b6eb959
harris <harris.kunakira@gmail.com>
2023-07-04 01:45:35 +0700

    wip: theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: 'hsl(var(--color-hsl-primary) / <alpha-value>)',
+      secondary: 'hsl(var(--color-hsl-secondary) / <alpha-value>)',
+      tertiary: 'hsl(var(--color-hsl-tertiary) / <alpha-value>)',
+      quaternary: 'hsl(var(--color-hsl-quaternary) / <alpha-value>)',
+      quinary: 'hsl(var(--color-hsl-quinary) / <alpha-value>)',
     },
+    extend: {},
   },
   corePlugins: {
     preflight: false,
