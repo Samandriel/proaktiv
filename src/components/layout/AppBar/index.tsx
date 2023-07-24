b8b511e0e668ebbd4782e34cf07862d20d015b96
harris <harris.kunakira@gmail.com>
2023-07-25 01:35:29 +0700

    feat: AppBar and AppLeftSidebar style

--- src/components/layout/AppBar/index.tsx
+++ src/components/layout/AppBar/index.tsx
@@ -1,55 +1,105 @@
+import { v4 as uuid } from 'uuid';
 import { Icon } from '@iconify/react';
 import styles from './AppBar.module.scss';
-
+import './AppBar.scss';
 import {
+  ActionIcon,
   Avatar,
   Group,
   Header,
+  Indicator,
   Menu,
+  TextInput,
   UnstyledButton,
   rem,
 } from '@mantine/core';
 
+const userMenuItems: {
+  id: string;
+  type: 'menu' | 'divider';
+  [key: string]: string;
+}[] = [
+  {
+    id: uuid(),
+    type: 'menu',
+    label: 'preferences',
+    icon: 'tabler:settings',
+    link: '',
+  },
+  { id: uuid(), type: 'divider' },
+  {
+    id: uuid(),
+    type: 'menu',
+    label: 'logout',
+    icon: 'tabler:logout',
+    link: '',
+  },
+];
+
 export const AppBar = () => {
+  const userMenuItemsElements = userMenuItems.map((item) => {
+    if (item.type === 'divider') {
+      return <Menu.Divider key={item.id} />;
+    }
+    return (
+      <Menu.Item key={item.id}>
+        <Icon icon={item.icon} />
+        {item.label}
+      </Menu.Item>
+    );
+  });
+
   return (
-    <Header height={rem(60)} className={styles.appBar}>
+    <Header height={rem(64)} className={styles.appBar} unstyled>
       <div className="w-full h-full flex items-center">
         <div className={styles.appBarItems}>
           <h1>ProAktiv</h1>
         </div>
         <div className={styles.appBarItemsEnd}>
+          <TextInput
+            icon={<Icon icon="tabler:search" />}
+            radius="xl"
+            size="md"
+            rightSection={
+              <ActionIcon size={32} radius="xl" variant="filled">
+                <Icon icon="tabler:arrow-right" />
+              </ActionIcon>
+            }
+            placeholder="Search"
+            rightSectionWidth={42}
+          />
+          <Indicator
+            inline
+            size={16}
+            offset={7}
+            position="top-end"
+            color="red"
+            withBorder
+          >
+            <Avatar radius="xl" className="app-bar__icon-button">
+              <Icon icon="tabler:bell" className={styles.appBarIconButton} />
+            </Avatar>
+          </Indicator>
           <Menu
             width={260}
             position="bottom-end"
             transitionProps={{ transition: 'pop-top-right' }}
             withinPortal
           >
-            {/* onClose={() => setUserMenuOpened(false)}
-          onOpen={() => setUserMenuOpened(true)} */}
             <Menu.Target>
               <UnstyledButton>
                 <Group spacing={7}>
-                  <Avatar src={''} alt={''} radius="xl" size={20} />
+                  <Avatar radius="xl" className="app-bar__icon-button">
+                    <Icon icon="tabler:user" />
+                  </Avatar>
                 </Group>
               </UnstyledButton>
             </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item>Liked posts</Menu.Item>
-              <Menu.Item>Saved posts</Menu.Item>
-              <Menu.Item>Your comments</Menu.Item>
-
-              <Menu.Label>Settings</Menu.Label>
-              <Menu.Item>Account settings</Menu.Item>
-              <Menu.Item>Change account</Menu.Item>
-              <Menu.Item>Logout</Menu.Item>
-
-              <Menu.Divider />
-
-              <Menu.Label>Danger zone</Menu.Label>
-              <Menu.Item>Pause subscription</Menu.Item>
-              <Menu.Item>Delete account</Menu.Item>
-            </Menu.Dropdown>
+            <Menu.Dropdown>{userMenuItemsElements}</Menu.Dropdown>
           </Menu>
+          <Avatar radius="xl" className="app-bar__icon-button">
+            <Icon icon="tabler:moon" />
+          </Avatar>
         </div>
       </div>
     </Header>
--- src/components/layout/AppLeftSidebar/index.tsx
+++ src/components/layout/AppLeftSidebar/index.tsx
@@ -1,5 +1,6 @@
+import { v4 as uuid } from 'uuid';
 import { Icon } from '@iconify/react';
-import { Navbar, rem } from '@mantine/core';
+import { Avatar, Menu, Navbar, UnstyledButton, rem } from '@mantine/core';
 import Link from 'next/link';
 import styles from './AppLeftSidebar.module.scss';
 
@@ -9,6 +10,26 @@ const menuItems = [
   { label: 'Todo', icon: 'bx:bx-list-check', link: '/todos' },
   { label: 'Notes', icon: 'bx:bx-notepad', link: '/notes' },
   { label: 'Toolbox', icon: 'bx:bx-wrench', link: '/toolbox' },
+];
+const userMenuItems: {
+  id: string;
+  type: 'menu' | 'divider';
+  [key: string]: string;
+}[] = [
+  {
+    id: uuid(),
+    type: 'menu',
+    label: 'Support',
+    icon: 'tabler:headphones',
+    link: '',
+  },
+  {
+    id: uuid(),
+    type: 'menu',
+    label: 'Feedback',
+    icon: 'tabler:request-quote',
+    link: '',
+  },
 ];
 
 export const AppLeftSidebar = () => {
@@ -30,11 +51,26 @@ export const AppLeftSidebar = () => {
     );
   });
 
+  const userMenuList = userMenuItems.map((item) => {
+    return (
+      <Link
+        href={item.link}
+        key={item.id}
+        className={styles.appLeftSidebarMenuItem}
+      >
+        <Icon className={styles.appLeftSidebarMenuItemIcon} icon={item.icon} />
+        <span>{item.label}</span>
+      </Link>
+    );
+  });
+
   return (
-    <Navbar width={{ base: rem(300) }} className={styles.appLeftSidebar}>
+    <Navbar className={styles.appLeftSidebar} unstyled>
       <Navbar.Section>{}</Navbar.Section>
       <Navbar.Section grow>{menuList}</Navbar.Section>
-      <Navbar.Section>{}</Navbar.Section>
+      <Navbar.Section className={styles.menuFooter}>
+        {userMenuList}
+      </Navbar.Section>
     </Navbar>
   );
 };
