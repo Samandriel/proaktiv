7051499b0ca32c73c78b7b268d9c07c2b26c69b0
harris <harris.kunakira@gmail.com>
2023-07-25 02:07:34 +0700

    feat: add app pages

---
+++ src/app/app/page.tsx
@@ -0,0 +1,11 @@
+export default function AppPage() {
+  return (
+    <main>
+      <h1 className="mb-4 text-4xl font-bold">Welcome ProAktiv</h1>
+      <p className="mb-4 text-lg">
+        ProAktiv is a platform that helps you to manage your tasks, track you
+        habbits and organize your life.
+      </p>
+    </main>
+  );
+}
---
+++ src/app/app/toolbox/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ToolboxPage() {
+  return <div>ToolboxPage</div>;
+}
